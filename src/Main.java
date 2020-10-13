29f050f28bc2324de868875abd82b59149f15fa0
prashathc <prashathc@gmail.com>
2020-10-12 20:46:26 -0400

    exercise working

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int[] arrayOfInts = new int[100];
+
+	for (int i = 0; i < arrayOfInts.length; i++) {
+	    arrayOfInts[i] = (int)(Math.random() * 5 + 1);
+    }
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Please enter an index of the array between 0 to 99");
+	int userIndex = input.nextInt();
+
+		try
+		{
+			System.out.println("Item at that index is: " + arrayOfInts[userIndex]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+
     }
 }
